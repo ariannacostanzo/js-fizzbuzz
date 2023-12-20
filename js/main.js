e7a84a5bcb79d1ffaf9c7d953f9c79f69b56eb19
ariannacostanzo <costanzo.arianna@outlook.it>
2023-12-20 16:55:58 +0100

    finished

--- js/main.js
+++ js/main.js
@@ -23,26 +23,26 @@ console.log('JS OK');
 
 // METODO template literals
 
-const row = document.querySelector('.row');
-let square = ''
+// const row = document.querySelector('.row');
+// let square = ''
 
 
-// ciclo for
-for (let i = 1; i <= 100; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-       square += "<div class='square bg-fizz-buzz'>FizzBuzz</div>"
-    } else if (i % 3 === 0) {
-        square += "<div class='square bg-fizz'>Fizz</div>"
-    } else if (i % 5 === 0) {
-        square += "<div class='square bg-buzz'>Buzz</div>"
-    }  else {
-        square += `<div class='square'>${i}</div>` 
+// // ciclo for
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//        square += "<div class='square bg-fizz-buzz'>FizzBuzz</div>"
+//     } else if (i % 3 === 0) {
+//         square += "<div class='square bg-fizz'>Fizz</div>"
+//     } else if (i % 5 === 0) {
+//         square += "<div class='square bg-buzz'>Buzz</div>"
+//     }  else {
+//         square += `<div class='square'>${i}</div>` 
         
-    }
+//     }
  
-}
+// }
 
-row.innerHTML = square
+// row.innerHTML = square
 
 
 
@@ -50,29 +50,30 @@ row.innerHTML = square
 
 // METODO manipolazione del DOM
 
-// const row = document.querySelector('.row');
-// // ciclo for
+const row = document.querySelector('.row');
+// ciclo for
 
-// for (let i = 1; i <= 100; i++) {
-//     const square = document.createElement('div');
-//     square.classList.add('square');
-//     const text = document.createElement('p')
+for (let i = 1; i <= 100; i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    const text = document.createElement('p')
     
 
-//     if (i % 3 === 0 && i % 5 === 0) {
-//        text.append('FizzBuzz') 
-//        square.classList.add('bg-fizz-buzz')
-//     } else if (i % 3 === 0) {
-//         text.append('Fizz') 
-//         square.classList.add('bg-fizz')
-//     } else if (i % 5 === 0) {
-//         text.append('Buzz') 
-//         square.classList.add('bg-buzz')
-//     }  else {
-//         text.append(i)
-//     }
+    if (i % 3 === 0 && i % 5 === 0) {
+       text.append('FizzBuzz') 
+       square.classList.add('bg-fizz-buzz')
+    } else if (i % 3 === 0) {
+        text.append('Fizz') 
+        square.classList.add('bg-fizz')
+    } else if (i % 5 === 0) {
+        text.append('Buzz') 
+        square.classList.add('bg-buzz')
+    }  else {
+        text.append(i)
+    }
 
-//     square.appendChild(text)
-//     row.appendChild(square);
- 
-// }
+    square.appendChild(text)
+    row.appendChild(square);
+    
+}
+
